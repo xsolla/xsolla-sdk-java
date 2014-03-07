4279e8083060c1f661220ec7590240d7e9f167d8
Alexander Bezrukov <a.bezrukov@xsolla.com>
2014-03-07 14:54:58 +0600

    Version 1.0.1.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## 1.0.1 (2014-03-07)
+* No hang on return from the application. Using `URLEncoder` instead of `AsynctHttpClient` in `UrlBuilder`.
+* Fix tests for JDK 8.
--- sdk/src/main/java/com/xsolla/sdk/Version.java
+++ sdk/src/main/java/com/xsolla/sdk/Version.java
@@ -3,7 +3,7 @@ package com.xsolla.sdk;
 public class Version {
 
     protected static final String name = "sdk-java";
-    protected static final String version = "1.0.0";
+    protected static final String version = "1.0.1";
 
     public String toString() {
         return String.format("%s/%s jre/%s", name, version, System.getProperty("java.version"));
